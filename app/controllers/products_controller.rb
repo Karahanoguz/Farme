094fe5a31224bf4ad1a5d4ea7887720c8d573ec8
Valentina Sapigni <vale-sapi@hotmail.it>
2021-06-02 12:12:41 +0200

    index feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,9 @@
 class ProductsController < ApplicationController
+  def index
+    if params[:query].present?
+      @products = Product.where(location: params[:query])
+    else
+      @products = Product.all
+    end
+  end
 end
