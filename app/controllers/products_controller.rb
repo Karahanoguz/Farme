a670a616e828420119ced916330148d2af233344
Valentina Sapigni <vale-sapi@hotmail.it>
2021-06-03 11:23:31 +0200

    searchbar fixed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,8 @@
 class ProductsController < ApplicationController
   def index
     if params[:search].present?
-      @products = Product.where("name ILIKE?", "%#{params[:query]}")
+      # @products = Product.where("name ILIKE?", "%#{params[:query]}")
+      @products = Product.search_by_name(params[:search][:query])
     else
       @products = Product.all
     end
