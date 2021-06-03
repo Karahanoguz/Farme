b87a16895c92520816c519983bd374285488c0af
Valentina Sapigni <vale-sapi@hotmail.it>
2021-06-03 13:10:55 +0200

    map implemented

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,13 +22,18 @@ require("channels")
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
 
+//CSS
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 // External imports
 import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+   initMapbox();
 });
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,9 +4,12 @@ class Product < ApplicationRecord
 
   belongs_to :user
 
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
   include PgSearch::Model
-  pg_search_scope :search_by_name,
-    against: [ :name ],
+  pg_search_scope :search_by_name_and_location,
+    against: [ :name, :location],
     using: {
       tsearch: { prefix: true } # <-- now `superman batm` will return something!
     }
