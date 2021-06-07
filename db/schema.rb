46e3d329f4dbfea854d951467974c0bc314940e5
Valentina Sapigni <vale-sapi@hotmail.it>
2021-06-07 17:52:30 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_06_07_142800) do
-=======
-ActiveRecord::Schema.define(version: 2021_06_07_130844) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
